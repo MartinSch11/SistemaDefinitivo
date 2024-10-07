c6ade913faaef4d5698e5e16b06f1dc0368c2328
Sofia Lopez <lopezzsofia19@gmail.com>
2024-10-07 11:39:49 -0300

    Avances en Agenda

--- src/main/java/controller/AgendaController.java
+++ src/main/java/controller/AgendaController.java
@@ -12,9 +12,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 
-import java.awt.*;
 import java.awt.ScrollPane;
-import java.awt.TextArea;
+import javafx.scene.control.TextArea;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -23,16 +22,19 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Optional;
 import javafx.scene.text.Text;
-
+import utilities.Paths;
+import utilities.SceneLoader;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 public class AgendaController {
     @FXML
     private Pane paneAgregarTareaPendiente;
     @FXML
-    private Spinner<Integer> Hora;
-    @FXML
-    private Spinner<Integer> Minutos;
-    @FXML private RadioButton radioNo; @FXML private RadioButton radioSi; @FXML private RadioButton radioDuranteDia;
+    private Spinner<Integer> Hora; @FXML private Spinner<Integer> Minutos;
+    @FXML private RadioButton radioNo; @FXML private RadioButton radioSi;
+
     @FXML
     private ComboBox<String> cmbEmpleadoTarea;
     @FXML
@@ -44,54 +46,22 @@ public class AgendaController {
     @FXML
     private Label txtFechaSemanal;
     @FXML
-    private LocalDate today = LocalDate.now(); //antes estaba sin el localdate.now
+    private LocalDate today = LocalDate.now();
 
     @FXML
-    private TableView<?> tablaAgenda;
-    @FXML
-    private TableColumn<?, ?> juevesCol;
-    @FXML
-    private TableColumn<?, ?> viernesCol;
-    @FXML
-    private TableColumn<?, ?> lunesCol;
-    @FXML
-    private TableColumn<?, ?> martesCol;
-    @FXML
-    private TableColumn<?, ?> miercolesCol;
-    @FXML
-    private TableColumn<?, ?> sabadoCol;
-    @FXML
-    private TableColumn<?, ?> domingoCol;
+    private String pendiente; @FXML private String empleadoTarea; @FXML private Integer horaPendiente; @FXML private Integer minutoPendiente;
 
-    @FXML
-    private String pendiente;
-    @FXML
-    private String empleadoTarea;
-    @FXML
-    private Integer horaPendiente; @FXML private Integer minutoPendiente;
     @FXML
     private LocalDate fechaPendiente;
     @FXML
     private GridPane gridPlanillaSemanal;
-    @FXML
-    private StackPane paneDomingo;
-    @FXML
-    private StackPane paneJueves;
-    @FXML
-    private StackPane paneLunes;
-    @FXML
-    private StackPane paneMartes;
-    @FXML
-    private StackPane paneMiercoles;
-    @FXML
-    private StackPane paneSabado;
-    @FXML
-    private StackPane paneViernes;
+
+    @FXML private StackPane paneDomingo; @FXML private StackPane paneJueves; @FXML private StackPane paneLunes; @FXML private StackPane paneMartes; @FXML private StackPane paneMiercoles; @FXML private StackPane paneSabado; @FXML private StackPane paneViernes;
 
 
     @FXML
     private void initialize() {
-        paneAgregarTareaPendiente.setVisible(false); //oculta el pane de agregar tarea
+        paneAgregarTareaPendiente.setVisible(false);
         radioNo.setSelected(true);
 
         // Inicializar la fecha actual
@@ -132,16 +102,21 @@ public class AgendaController {
         // Calcular el lunes de la semana actual
         LocalDate monday = today.with(DayOfWeek.MONDAY);
 
-        // Llamar al método que actualiza los panes con los días de la semana
+        // Llamar al metodo que actualiza los panes con los días de la semana
         setDaysInWeek(monday);
 
         // Configurar la fila 0 del GridPane
         RowConstraints row0 = new RowConstraints();
-        row0.setPrefHeight(120); // Altura preferida
-        row0.setMinHeight(120);  // Altura mínima
-        row0.setMaxHeight(120);  // Altura máxima
+        row0.setPrefHeight(120);
+        row0.setMinHeight(120);
+        row0.setMaxHeight(120);
         gridPlanillaSemanal.getRowConstraints().add(0, row0);
 
+    }
+
+    @FXML
+    void handleVolver(ActionEvent event) {
+        SceneLoader.handleVolver(event, Paths.CAJERA_MAINMENU, "/com/example/pasteleria/MainMenuCajera.fxml", true);
     }
 
     @FXML
@@ -158,6 +133,112 @@ public class AgendaController {
         today = today.plusWeeks(1);
         // Actualizar la fecha semanal y los StackPane
         FechaSemanal(today);
+    }
+
+    // Función que habilita/deshabilita los Spinner según el RadioButton seleccionado
+    private void actualizarEstadoSpinner() {
+        if (radioSi.isSelected()) {
+            Hora.setDisable(false);   // Habilitar Spinner Hora
+            Minutos.setDisable(false); // Habilitar Spinner Minutos
+            radioNo.setDisable(false);
+        } else if (radioNo.isSelected()) {
+            Hora.setDisable(true);    // Deshabilitar Spinner Hora
+            Minutos.setDisable(true);  // Deshabilitar Spinner Minutos
+            radioSi.setDisable(false);
+        }
+    }
+
+    private boolean validarCamposObligatorios() {
+        boolean valid = true;
+        if (dateDiaTarea.getValue() == null) {
+            valid = false;
+        }
+        if (tareaPendiente.getText() == null || tareaPendiente.getText().trim().isEmpty()) {
+            valid = false;
+        }
+        if (radioSi.isSelected() && (Hora.getValue() == null || Hora.getValue() == 0) && (Minutos.getValue() == null || Minutos.getValue() == 0)) {
+            valid = false;
+        }
+        if (cmbEmpleadoTarea.getValue() == null || cmbEmpleadoTarea.getValue().toString().trim().isEmpty()) {
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    private void vaciarCamposNuevaTarea() {
+        // Restablecer Spinner de Hora
+        Hora.getValueFactory().setValue(Hora.getValueFactory().getConverter().fromString("8")); // Suponiendo que 8 es el valor inicial de las horas
+        // Restablecer Spinner de Minutos a su valor inicial
+        Minutos.getValueFactory().setValue(Minutos.getValueFactory().getConverter().fromString("0"));
+
+        radioSi.setSelected(false);
+        radioNo.setSelected(true);
+        tareaPendiente.clear();
+        cmbEmpleadoTarea.setValue(null);
+        dateDiaTarea.setValue(null);
+    }
+
+    @FXML
+    private Button btnNuevaTarea;
+    @FXML
+    void NuevaTarea(ActionEvent event) {
+        paneAgregarTareaPendiente.setVisible(true);
+        btnNuevaTarea.setDisable(true);
+
+        //guardar los datos de la Nueva Tarea
+
+        //el radiobuttonNO  NO funciona en el caso de seleccionar y guardar la tareaPendiente
+
+        //eliminar tareas hechas mediante una opción de "Marcar como hecho"
+
+        //guardar LocalDate, guardar la fecha
+
+        //añadir a la base de datos las tareas pendientes para que se guarden al salir de Agenda
+
+    }
+
+    @FXML
+    void cancelarTarea(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmación");
+        alert.setHeaderText("Se perderán los cambios realizados. ¿Desea salir?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            vaciarCamposNuevaTarea();
+            paneAgregarTareaPendiente.setVisible(false);
+            btnNuevaTarea.setDisable(false);
+        }
+    }
+
+    @FXML
+    void guardarTarea(ActionEvent event) {
+        if (validarCamposObligatorios()) {
+            btnNuevaTarea.setDisable(false);
+            //tarea pendiente
+            pendiente = tareaPendiente.getText();
+            empleadoTarea = cmbEmpleadoTarea.getValue();
+            fechaPendiente = dateDiaTarea.getValue();
+            if (radioSi.isSelected()) {
+                horaPendiente = Hora.getValue();
+                minutoPendiente = Minutos.getValue();
+            }
+
+            vaciarCamposNuevaTarea();
+
+            paneAgregarTareaPendiente.setVisible(false);
+
+            insertarNuevaTarea(empleadoTarea, fechaPendiente, horaPendiente, minutoPendiente, pendiente);
+
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Advertencia");
+            alert.setHeaderText("Campos obligatorios sin completar.");
+            alert.setContentText("Solución: completar los campos vacíos.");
+            alert.showAndWait();
+        }
     }
 
     public void FechaSemanal(LocalDate selectedDate) {
@@ -189,8 +270,8 @@ public class AgendaController {
             LocalDate currentDay = startOfWeek.plusDays(i);
 
             // Crear el texto con formato
-            String dayOfWeek = currentDay.format(dayFormatter).toUpperCase();  // Nombre del día de la semana en mayúsculas
-            String fullDate = currentDay.format(dateFormatter).toUpperCase();  // Fecha con día y mes en mayúsculas
+            String dayOfWeek = currentDay.format(dayFormatter).toUpperCase();
+            String fullDate = currentDay.format(dateFormatter).toUpperCase();
 
             Text text = new Text(dayOfWeek + "\n" + fullDate);
             text.setStyle("-fx-font-size: 16px; -fx-font-weight: bold; -fx-fill: white; -fx-text-alignment: center;");
@@ -202,150 +283,43 @@ public class AgendaController {
         }
     }
 
-    // Función que habilita/deshabilita los Spinner según el RadioButton seleccionado
-    private void actualizarEstadoSpinner() {
-        if (radioSi.isSelected()) {
-            Hora.setDisable(false);   // Habilitar Spinner Hora
-            Minutos.setDisable(false); // Habilitar Spinner Minutos
-            radioNo.setDisable(false);
-        } else if (radioNo.isSelected()) {
-            Hora.setDisable(true);    // Deshabilitar Spinner Hora
-            Minutos.setDisable(true);  // Deshabilitar Spinner Minutos
-            radioSi.setDisable(false);
+    private StackPane getStackPaneForDay(int dayOfWeek) {
+        switch (dayOfWeek) {
+            case 1: return paneLunes;
+            case 2: return paneMartes;
+            case 3: return paneMiercoles;
+            case 4: return paneJueves;
+            case 5: return paneViernes;
+            case 6: return paneSabado;
+            case 7: return paneDomingo;
+            default: throw new IllegalArgumentException("Día de la semana inválido: " + dayOfWeek);
         }
     }
 
-    private boolean validarCamposObligatorios() {
-        boolean valid = true;
-        if (dateDiaTarea.getValue() == null) {
-            valid = false;
-        }
-        /*if (tareaPendiente.getText() == null || tareaPendiente.getText().trim().isEmpty()) {
-            valid = false;
-        }*/
-        if (radioSi.isSelected() && (Hora.getValue() == null || Hora.getValue() == 0) && (Minutos.getValue() == null || Minutos.getValue() == 0)) {
-            valid = false;
-        }
-        if (cmbEmpleadoTarea.getValue() == null || cmbEmpleadoTarea.getValue().toString().trim().isEmpty()) {
-            valid = false;
-        }
-
-        return valid;
-    }
-
-    private void vaciarCamposNuevaTarea() {
-        // Restablecer Spinner de Hora
-        Hora.getValueFactory().setValue(Hora.getValueFactory().getConverter().fromString("8")); // Suponiendo que 8 es el valor inicial de las horas
-        // Restablecer Spinner de Minutos a su valor inicial
-        Minutos.getValueFactory().setValue(Minutos.getValueFactory().getConverter().fromString("0"));
-
-        // Si tienes otros campos, como TextField, también los vacías
-        //tareaPendiente.setText("");  // Establece el contenido a una cadena vacía
-        radioSi.setSelected(false);
-        radioNo.setSelected(true);
-
-        cmbEmpleadoTarea.setValue(null);
-        dateDiaTarea.setValue(null);
-    }
-
-
-    private void guardarDatosNuevaTarea() {
-
-
-    }
-
-
-    @FXML
-    private Button btnNuevaTarea;
-
-    @FXML
-    void NuevaTarea(ActionEvent event) {
-        paneAgregarTareaPendiente.setVisible(true);
-        btnNuevaTarea.setDisable(true);
-
-        //guardar los datos de la Nueva Tarea
-
-        //el radiobuttonNO  NO funciona en el caso de seleccionar y guardar la tareaPendiente
-
-        //falta agregar listview con la nueva tarea
-
-        //eliminar tareas hechas mediante una opción de "Marcar como hecho"
-
-    }
-
-
-    @FXML
-    void cancelarTarea(ActionEvent event) {
-        Node source = (Node) event.getSource();
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Confirmación");
-        alert.setHeaderText("Se perderán los cambios realizados. ¿Desea salir?");
-
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.isPresent() && result.get() == ButtonType.OK) {
-            vaciarCamposNuevaTarea();
-            paneAgregarTareaPendiente.setVisible(false);
-            btnNuevaTarea.setDisable(false);
-        }
-    }
-
-    @FXML
-    void guardarTarea(ActionEvent event) {
-        if (validarCamposObligatorios()) {
-            btnNuevaTarea.setDisable(false);
-
-            empleadoTarea = cmbEmpleadoTarea.getValue();
-            fechaPendiente = dateDiaTarea.getValue();
-            if (radioSi.isSelected()) {
-                horaPendiente = Hora.getValue();
-                minutoPendiente = Minutos.getValue();
-            }
-            //tarea pendiente
-
-            vaciarCamposNuevaTarea();
-
-            paneAgregarTareaPendiente.setVisible(false);
-//--------------funcion de guardar datos de la nueva tarea-------------------
-            insertarNuevaTarea(empleadoTarea, fechaPendiente, horaPendiente, minutoPendiente);
-            //guardarDatosNuevaTarea();
-
-        } else {
-            Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setTitle("Advertencia");
-            alert.setHeaderText("Campos obligatorios sin completar.");
-            alert.setContentText("Solución: completar los campos vacíos.");
-            alert.showAndWait();
-        }
-    }
-
-    //NO funciona que se quede estatico tamaño fila 0
-    //luego de 5 stackPane de 120px se debe crear un rollbar
-
-    //---------------------otra forma de crear la nueva tarea--------------
-    // Este método creará una nueva tarea y la insertará en el GridPane
-    private void insertarNuevaTarea(String empleadoTarea, LocalDate fechaPendiente, int horaPendiente, int minutoPendiente) {
-        StackPane tareaPane = new StackPane();
-        Label tareaLabel = new Label(empleadoTarea + "\n" + String.format("%02d:%02d hs", horaPendiente, minutoPendiente));
+    private void insertarNuevaTarea(String empleadoTarea, LocalDate fechaPendiente, int horaPendiente, int minutoPendiente, String pendiente) {
+        Label tareaLabel = new Label(pendiente + "\n" + empleadoTarea + "\n" + String.format("%02d:%02d hs", horaPendiente, minutoPendiente));
         tareaLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold; -fx-fill: black; -fx-text-alignment: left; -fx-padding-top: 5px");
-        tareaPane.getChildren().add(tareaLabel);
+        tareaLabel.setWrapText(true); // No permitir que el texto se ajuste al ancho del TextArea
 
-        // Establecer el tamaño del StackPane
-        tareaPane.setPrefHeight(120);
-        tareaPane.setMaxHeight(120);
-        tareaPane.setMinHeight(120);
+        StackPane tareaPane = new StackPane(tareaLabel);
+        tareaPane.setPrefHeight(150);
+        tareaPane.setMaxHeight(150);
+        tareaPane.setMinHeight(150);
         tareaPane.setStyle("-fx-background-color: #FFF4F4; -fx-effect: dropshadow(gaussian, rgba(0, 0, 0, 0.3), 10, 0.0, 2, 2); -fx-border-radius: 5px; -fx-background-radius: 5px; -fx-margin-top: 5px;");
+
+        // Obtener la columna correspondiente al día de la semana de fechaPendiente
+        int columnIndex = fechaPendiente.getDayOfWeek().getValue() - 1;
+
         // Determinar la siguiente fila en el GridPane
-        int newRowIndex = gridPlanillaSemanal.getRowConstraints().size(); // Obtener el número de filas actuales
-        // Insertar el StackPane en el GridPane
-        gridPlanillaSemanal.add(tareaPane, 0, newRowIndex);
+        int newRowIndex = gridPlanillaSemanal.getRowConstraints().size();
+        gridPlanillaSemanal.add(tareaPane, columnIndex, newRowIndex); // Insertar el StackPane en la nueva fila y columna correspondiente
 
         // Añadir una restricción de tamaño fijo a la nueva fila
         RowConstraints rowConstraints = new RowConstraints();
-        rowConstraints.setPrefHeight(120);
-        rowConstraints.setMaxHeight(120);
-        rowConstraints.setMinHeight(120);
+        rowConstraints.setPrefHeight(150);
+        rowConstraints.setMaxHeight(150);
+        rowConstraints.setMinHeight(150);
         gridPlanillaSemanal.getRowConstraints().add(rowConstraints);
-
-
     }
+
 }
